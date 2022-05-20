ca56e94c5ba3014352ee18fa9c5188fc8266aaa1
almoslembs <almoslembs@gmail.com>
2022-05-20 22:15:41 +0700

    tst

---
+++ Object Repository/testlast/Page_Enterprise  A smarter way to work toge_b969cd/a_Contact Sales.rs
@@ -0,0 +1,143 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Contact Sales</name>
+   <tag></tag>
+   <elementGuidId>36d50ffe-acdf-4f6e-81f2-04d4b6956acf</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>a.btn-mktg.ml-2.ml-md-0.btn-muted-mktg</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//a[contains(text(),'Contact Sales')])[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>5b724622-5787-40c0-887e-0e5c4eb5e4d8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn-mktg ml-2 ml-md-0 btn-muted-mktg</value>
+      <webElementGuid>6b1cba49-fe51-4b6b-8d4a-5eae66814e98</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-analytics-event</name>
+      <type>Main</type>
+      <value>{&quot;category&quot;:&quot;Contact Sales&quot;,&quot;action&quot;:&quot;click to Contact Sales&quot;,&quot;label&quot;:&quot;ref_page:/enterprise;ref_cta:Contact Sales;&quot;}</value>
+      <webElementGuid>d51632ed-bf05-4abc-8640-45a5a6f321f5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://github.com/enterprise/contact?ref_page=/enterprise&amp;ref_cta=Contact%20Sales&amp;ref_loc=hero</value>
+      <webElementGuid>a44fef91-b8a7-4137-9b74-8a84393e8a05</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+  Contact Sales
+  
+  
+</value>
+      <webElementGuid>b63d4438-a74a-4fbd-82a5-f13bc32c6d29</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;logged-out env-production page-responsive header-black overflow-x-hidden intent-mouse&quot;]/div[@class=&quot;application-main&quot;]/main[@class=&quot;font-mktg&quot;]/div[@class=&quot;enterprise-hero overflow-hidden&quot;]/div[@class=&quot;container-xl mx-auto p-responsive position-relative&quot;]/div[@class=&quot;d-flex flex-items-center flex-justify-center flex-md-justify-between flex-wrap gutter-spacious position-relative&quot;]/div[@class=&quot;col-9-max position-relative z-2 ml-lg-4 ml-xl-0&quot;]/a[@class=&quot;btn-mktg ml-2 ml-md-0 btn-muted-mktg&quot;]</value>
+      <webElementGuid>a33f5166-fecc-45ad-8f68-982875bc2684</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>(//a[contains(text(),'Contact Sales')])[2]</value>
+      <webElementGuid>7bba3f70-1596-4c6d-a548-48cdfc0c56ea</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Start a free trial'])[1]/following::a[1]</value>
+      <webElementGuid>2371d279-64da-451f-be9f-86103bf9aeb4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='over 83 million developers'])[1]/following::a[2]</value>
+      <webElementGuid>220cfc46-4aa5-4397-94e9-1c59a165ce94</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Empower your team.Transform your business.'])[1]/preceding::a[1]</value>
+      <webElementGuid>afa80365-e272-46f2-ac19-39841d7cc0f6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Security'])[2]/preceding::a[1]</value>
+      <webElementGuid>a41b9ed5-f141-43e1-87fa-e2794ddac2bc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, 'https://github.com/enterprise/contact?ref_page=/enterprise&amp;ref_cta=Contact%20Sales&amp;ref_loc=hero')]</value>
+      <webElementGuid>a57e4b4a-5aad-493e-9c1f-7b77edebdf22</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a[2]</value>
+      <webElementGuid>d6c929db-b1f5-411e-b932-4b1bbef6838b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = 'https://github.com/enterprise/contact?ref_page=/enterprise&amp;ref_cta=Contact%20Sales&amp;ref_loc=hero' and (text() = '
+  Contact Sales
+  
+  
+' or . = '
+  Contact Sales
+  
+  
+')]</value>
+      <webElementGuid>a4115e94-4476-42ec-a06c-75c32ba6f3c2</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/testlast/Page_GitHub Where the world builds software_3c4b98/summary_Explore.rs
@@ -0,0 +1,116 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>summary_Explore</name>
+   <tag></tag>
+   <elementGuidId>86e4c616-a161-45ca-ae9b-a9391fbfb3e7</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Enterprise'])[1]/following::summary[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>summary</value>
+      <webElementGuid>b678145a-5fde-478c-beb8-14760f4319c2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>HeaderMenu-summary HeaderMenu-link px-0 py-3 border-0 no-wrap d-block d-lg-inline-block</value>
+      <webElementGuid>ff09c41b-8571-4916-ac5c-d6e616fc8ce8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+        Explore
+        
+      </value>
+      <webElementGuid>3346a42b-e065-47c8-9ac3-2e14fb54de13</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;logged-out env-production page-responsive header-overlay intent-mouse&quot;]/div[@class=&quot;position-relative js-header-wrapper&quot;]/header[@class=&quot;Header-old header-logged-out js-details-container Details position-relative f4 py-2&quot;]/div[@class=&quot;container-xl d-lg-flex flex-items-center p-responsive&quot;]/div[@class=&quot;HeaderMenu HeaderMenu--logged-out position-fixed top-0 right-0 bottom-0 height-fit position-lg-relative d-lg-flex flex-justify-between flex-items-center flex-auto&quot;]/nav[@class=&quot;mt-0 px-3 px-lg-0 mb-5 mb-lg-0&quot;]/ul[@class=&quot;d-lg-flex list-style-none&quot;]/li[@class=&quot;mr-0 mr-lg-3 position-relative flex-wrap flex-justify-between flex-items-center border-bottom border-lg-bottom-0 d-block d-lg-flex flex-lg-nowrap flex-lg-items-center&quot;]/details[@class=&quot;HeaderMenu-details details-overlay details-reset width-full&quot;]/summary[@class=&quot;HeaderMenu-summary HeaderMenu-link px-0 py-3 border-0 no-wrap d-block d-lg-inline-block&quot;]</value>
+      <webElementGuid>10ebe61a-ad9a-4d40-88b9-4b8b74d31953</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Enterprise'])[1]/following::summary[1]</value>
+      <webElementGuid>e5468f1d-9267-4594-80a7-40f1daacf70c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Team'])[1]/following::summary[1]</value>
+      <webElementGuid>b538daf8-a9d2-402b-9fed-cebf4e8162aa</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Explore GitHub'])[1]/preceding::summary[1]</value>
+      <webElementGuid>16b3a96c-6eee-4540-ad83-9e140b614d36</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Learn and contribute'])[1]/preceding::summary[1]</value>
+      <webElementGuid>1ae47c8a-f955-4d38-bb59-7e0bf6504fd3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Explore']/parent::*</value>
+      <webElementGuid>89380bac-ee3f-4bcf-9be8-30e73077d817</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[4]/details/summary</value>
+      <webElementGuid>094739ab-f6a4-4647-8bcf-9ddb3b64b9e0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//summary[(text() = '
+        Explore
+        
+      ' or . = '
+        Explore
+        
+      ')]</value>
+      <webElementGuid>cb057d8e-e70c-40a4-a98a-fb99d302884c</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
